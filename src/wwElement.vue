e7e03f0f59434fcf2d91c90848e66010269554c8
Šefčík Lukáš <sefcik@kros.sk>
2024-07-15 14:16:45 +0200

    Css.

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -24,7 +24,7 @@
 
 <script>
 import "./dx.fluent.dx-light-theme.css";
-import "./dx.light.css";
+import "./dx.fluent.dx-light-theme-new.css";
 import "./dx-gantt.min.css";
 
 import {DxColumn, DxGantt, DxTasks} from 'devextreme-vue/gantt';
